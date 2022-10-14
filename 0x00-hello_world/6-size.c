c1a55a5ab9b3b5aa504d5553954200dfb6475c10
sarijblack <ngoziawurihappy@gmail.com>
2022-10-14 12:52:37 -0700

    My first commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Alwqys success
+ */
+int main(void)
+{	
+	printf("the size of various types on the computer it is compiled and run on"\n");
+	return(0);
+}
