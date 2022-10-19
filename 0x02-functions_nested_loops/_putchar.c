6d5f83cb25f578ce2acba9e8b87e8f3e7f7ca9aa
sarijblack <ngoziawurihappy@gmail.com>
2022-10-19 15:20:24 -0700

    My first commit

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -3,7 +3,7 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to point
+ * @c; the character to point
  *
  * Return: on success 1
  */
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,16 +2,16 @@
 #define MAIN.h
 
 void print_alphabet(void);
-void print_alphabet_10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void jack-bauer(void);
+void jack_bauer(void);
 void times_tables(void);
 int add(int, int);
 void print_to_98(int n);
-int -putchar(char c);
+int _putchar(char c);
 
 #endif
