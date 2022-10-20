5ec59f0ce0abc99204f831b8f06128c3b51f35b9
sarijblack <ngoziawurihappy@gmail.com>
2022-10-20 12:54:30 -0700

    My first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,21 +4,17 @@
 /*
  * main - Entry point
  * prints num from 0 to 9
- * Return: Always 0
+ * Return: Always succesd
  */
 int main(void)
 {
-	_putchar('0\n');
-	_putchar('1\n');
-	_putchar('2\n');
-	_putchar('3\n');
-	_putchar('4\n');
-	_putchar('5\n');
-	_putchar('6\n');
-	_putchar('7\n');
-	_putchar('8\n');
-	_putchar('9\n');
+	char a;
+	for (a = '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+	}
+		_putchar('\n');
 
-	return (0);
+		return (0);
 }
 
