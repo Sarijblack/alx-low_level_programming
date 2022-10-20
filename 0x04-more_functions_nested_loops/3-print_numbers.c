e7d3b3c7ec47ae03f4a81fdcf36725a159bf7912
sarijblack <ngoziawurihappy@gmail.com>
2022-10-20 13:17:28 -0700

    My first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,8 @@
 #include "main.h"
-#include <stdio>
 
 /*
  * main - check the code
- * prints num from 0 to 9
+ * prints_numbers from 0 to 9
  * Return: Always succesd
  */
 void print_numbers(void)
