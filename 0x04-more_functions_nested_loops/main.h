0f5b8fef6210b731927e7d8d048e580f6b1fc4cf
sarijblack <ngoziawurihappy@gmail.com>
2022-10-20 12:20:39 -0700

    My first commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,17 +1,16 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 
-extern void print_alphabet(void);
-extern void print_alphabet_x10(void);
-extern int _islower(int c);
-extern int _isalpha(int c);
-extern int print_sign(int n);
-extern int _abs(int);
-extern int print_last_digit(int);
-extern void jack_bauer(void);
-extern void times_tables(void);
-extern int add(int, int);
-extern void print_to_98(int n);
-extern int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif
