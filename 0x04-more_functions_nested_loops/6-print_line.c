646f9bcdd219716d398985522e0cf5b436d1a971
sarijblack <ngoziawurihappy@gmail.com>
2022-10-21 08:38:39 -0700

    My first commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
  * print_line - print a straight line using '_'
  * @n; the number of '_' to be printed
- * Return: success
  */
 void print_line(int n)
 {
@@ -11,7 +11,7 @@ void print_line(int n)
 
 	if (n > 0)
 	{
-		for (len = '0'; len < 'n'; len++)
+		for (len = 0; len < n; len++)
 			 _putchar('_');
 	}
 	_putchar('\n');
