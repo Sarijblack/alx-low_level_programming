9d54ecde9da178ab3a99a40e1c8acd0272c02407
sarijblack <ngoziawurihappy@gmail.com>
2022-10-21 05:41:22 -0700

    My first commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_numbers - print from 0 to 9
  * Return: Always 0.
  */
-int main(void)
+void print_numbers(void)
 {
-	int i = 48
-
-	if (i < 57)
-		_putchar(i++ < '57');
+	int a = 0
+	;while (a < 10)
+		_putcha(a++ + '0');
 
 	_putchar('\n');
+	
 	return (0);
 }
